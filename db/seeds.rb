128e91e7d9f27466e1e2b204b6da61ec386b6e4d
Torey Hickman <torey@toreyhickman.com>
2015-08-26 16:54:51 -0500

    Create entries in seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Entry.create [{ :singer => 'Elmo Tate',      :song_title => 'I Love Rock & Roll'},
+              { :singer => 'Chase Lee',      :song_title => 'Say It Ain\'t So'},
+              { :singer => 'Ileen Eliott',   :song_title => 'Poison'},
+              { :singer => 'Rasim Fournier', :song_title => 'Hold On'},
+              { :singer => 'Ella Weaver',    :song_title => 'One Way or Another'}]
