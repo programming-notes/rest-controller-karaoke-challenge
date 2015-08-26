42a73032f92312545aad7b1b575818f3da10ea06
Torey Hickman <torey@toreyhickman.com>
2015-08-26 17:11:11 -0500

    Add new entry

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -2,3 +2,12 @@ get '/all-the-entries' do
   @entries = Entry.all
   erb :'entries/index'
 end
+
+get '/new-entry-form' do
+  erb :'entries/new'
+end
+
+post '/create-new-post' do
+  entry = Entry.new(params[:entry])
+  redirect entry.save ? '/all-the-entries' : '/create-new-post'
+end
