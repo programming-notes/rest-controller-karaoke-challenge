916fe1c08f7cd338fe2f944f953b7f99a9a04871
Torey Hickman <torey@toreyhickman.com>
2015-08-27 08:34:56 -0500

    Show one entry

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -11,3 +11,8 @@ post '/create-new-post' do
   entry = Entry.new(params[:entry])
   redirect entry.save ? '/all-the-entries' : '/create-new-post'
 end
+
+get '/show-one-entry/:id' do
+  @entry = Entry.find(params[:id])
+  erb :'entries/show'
+end
