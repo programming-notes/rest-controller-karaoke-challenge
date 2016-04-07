18dfb470e5e2c997cbe7c259a6445411c808f367
Torey Hickman <torey@toreyhickman.com>
2016-04-07 11:39:16 -0500

    Remove validation specs for entry

--- spec/models/entry_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../spec_helper'
-
-describe Entry do
-  it { should validate_presence_of :singer }
-  it { should validate_length_of(:singer).is_at_most(64) }
-
-  it { should validate_presence_of :song_title }
-  it { should validate_length_of(:song_title).is_at_most(64)}
-end
